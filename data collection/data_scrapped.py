e90dfbf7c3fe27006a3b51fb9137596679b9aab3
Wasim M <66107425+m-wasim@users.noreply.github.com>
2021-01-27 12:48:07 +0530

    Update data_scrapped.py

--- data collection/data_scrapped.py
+++ data collection/data_scrapped.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 path = "C:\\Users\\Wasim\\Desktop\\Data_science_Projects\\datascience_salary\\data collection\\chromedriver.exe"
 rows=1000
-df=olx.get_information(path,7,4)
+df=olx.get_information(path,4,5) #4 states
 
 df.to_csv('olx_rent_data.csv', index = False)
